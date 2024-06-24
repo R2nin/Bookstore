a841e4cfefb1a923eba06a7201f2281b87cf856b
R2nin <arthur.miura@fema.edu.br>
2024-06-24 16:27:32 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -20,13 +20,14 @@
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+    $secret_key = "sk_test_51M94h5Hp65tXrQ3P4wJRttxoeJDyxOXhUc1p402Oc4fbmtzTK1VZ3yDF5kJKIty9Sf1ygA7WDevSht8Ba88SbSsk00GwlnUHF9";
 
     
 
-    //  if($conn) {
+    // if($conn) {
     //     echo "worked successfully";
-    //  } else {
-    //      echo "error in db connection";
-    //  }
+    // } else {
+    //     echo "error in db connection";
+    // }
 
 
