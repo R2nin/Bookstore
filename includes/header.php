6603e89716409e406c424626f4ab51e203edfc97
R2nin <arthur.miura@fema.edu.br>
2024-06-27 16:31:07 -0300

    Updates

--- includes/header.php
+++ includes/header.php
@@ -38,7 +38,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-dark" >
     <div class="container" style="margin-top: none">
-        <a class="navbar-brand  text-white" href="<?php echo APPURL; ?>">Bookstore</a>
+        <a class="navbar-brand  text-white" href="<?php echo APPURL; ?>">KooBE</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
